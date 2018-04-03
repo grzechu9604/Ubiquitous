73a4bc84d1d824c571019940554aa1506ad6c88a
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-04-03 19:28:02 +0200

    Testy Poprawa nazwy metody testującej + nowe metody

--- Calculator/app/src/test/java/com/example/grzegorz/calculator/InputStringTest.kt
+++ Calculator/app/src/test/java/com/example/grzegorz/calculator/InputStringTest.kt
@@ -17,8 +17,33 @@ class InputStringTest {
     }
 
     @Test
-    fun WrongEnterTest() {
+    fun CorrectAddTest() {
         input.addChar('9')
         Assert.assertEquals(9.0, input.toDouble())
+    }
+
+    @Test
+    fun IncorrectAddTest() {
+        input.addChar('a')
+        Assert.assertEquals(null, input.toDouble())
+        Assert.assertEquals("", input.toString())
+    }
+
+    @Test
+    fun CommaAddTest() {
+        input.addChar('.')
+        input.addChar('7')
+        Assert.assertEquals(0.7, input.toDouble())
+        Assert.assertEquals("0.7", input.toString())
+    }
+
+    @Test
+    fun CommaAddAndEraseTest() {
+        input.addChar('.')
+        input.addChar('7')
+        input.deleteLast()
+        input.addChar('8')
+        Assert.assertEquals(0.8, input.toDouble())
+        Assert.assertEquals("0.8", input.toString())
     }
 }
