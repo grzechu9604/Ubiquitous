6f93d2a0aea70cb0b5e349fcadce638b09ced31e
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-04-04 18:30:52 +0200

    GUI Implementacja metod obsługujących kliknięcia przycisków

--- Calculator/app/src/main/java/com/example/grzegorz/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/grzegorz/calculator/MainActivity.kt
@@ -5,8 +5,10 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,4 +40,153 @@ class MainActivity : AppCompatActivity() {
     }
 
     val rpnCalculator : RPNCalculator = RPNCalculator()
+    val inputString : InputString = InputString()
+
+    fun updateTextBoxes()
+    {
+        inputTextView.text = inputString.toString()
+        firstElementTextView.text = rpnCalculator.GetTopElement()?.toString()
+        secondElementTextView.text = rpnCalculator.GetSecondElement()?.toString()
+        thirdElementTextView.text = rpnCalculator.GetThirdElement()?.toString()
+    }
+
+    fun updateInputTextBox()
+    {
+        inputTextView.text = inputString.toString()
+    }
+
+    fun onEnterClick(v : View)
+    {
+        rpnCalculator.Enter(inputString.toString())
+        updateTextBoxes()
+    }
+
+    fun onSwapClick(v : View)
+    {
+        rpnCalculator.SwapTop()
+        updateTextBoxes()
+    }
+
+    fun onDropClick(v : View)
+    {
+        rpnCalculator.DropTop()
+        updateTextBoxes()
+    }
+
+    fun onClearAllClick(v : View)
+    {
+        rpnCalculator.ClearAll()
+        updateTextBoxes()
+    }
+
+
+    fun onSqrtClick(v : View)
+    {
+        rpnCalculator.Sqrt()
+        updateTextBoxes()
+    }
+
+
+    fun onPowClick(v : View)
+    {
+        rpnCalculator.Pow()
+        updateTextBoxes()
+    }
+
+
+    fun onDivideClick(v : View)
+    {
+        rpnCalculator.Divide()
+        updateTextBoxes()
+    }
+
+    fun onDiffrenceClick(v : View)
+    {
+        rpnCalculator.Diffrence()
+        updateTextBoxes()
+    }
+
+    fun onMultiplyClick(v : View)
+    {
+        rpnCalculator.Multiply()
+        updateTextBoxes()
+    }
+
+    fun onSumClick(v : View)
+    {
+        rpnCalculator.Sum()
+        updateTextBoxes()
+    }
+
+    fun onOneClick(v : View)
+    {
+        numberClicked('1')
+    }
+
+    fun onTwoClick(v : View)
+    {
+        numberClicked('2')
+    }
+
+    fun onThreeClick(v : View)
+    {
+        numberClicked('3')
+    }
+
+    fun onFourClick(v : View)
+    {
+        numberClicked('4')
+    }
+
+    fun onFiveClick(v : View)
+    {
+        numberClicked('5')
+    }
+
+    fun onSixClick(v : View)
+    {
+        numberClicked('6')
+    }
+
+    fun onSevenClick(v : View)
+    {
+        numberClicked('7')
+    }
+
+    fun onEightClick(v : View)
+    {
+        numberClicked('8')
+    }
+
+    fun onNineClick(v : View)
+    {
+        numberClicked('9')
+    }
+
+    fun onZeroClick(v : View)
+    {
+        numberClicked('0')
+    }
+
+    fun onCommaClick(v : View)
+    {
+        numberClicked('.')
+    }
+
+    fun onEraseClick(v : View)
+    {
+        inputString.deleteLast()
+        updateInputTextBox()
+    }
+
+    private fun numberClicked(c : Char)
+    {
+        inputString.addChar(c)
+        updateInputTextBox()
+    }
+
+
+
+
+
 }
