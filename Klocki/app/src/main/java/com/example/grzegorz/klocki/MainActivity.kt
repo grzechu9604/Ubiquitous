47e86a6407926c9ce562c2543c31ccfd9a3fb633
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-04-30 22:54:57 +0200

    Szkielet pobierania XMLa

--- Klocki/app/src/main/java/com/example/grzegorz/klocki/MainActivity.kt
+++ Klocki/app/src/main/java/com/example/grzegorz/klocki/MainActivity.kt
@@ -1,23 +1,55 @@
 package com.example.grzegorz.klocki
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
 class MainActivity : AppCompatActivity() {
+
+    private val AddNewProjectResult = 1001
+    private val UrlParameter = "URL_PARAMETER"
+
+    private var urlString : String = "http://fcds.cs.put.poznan.pl/MyWeb/BL/"
+
+    private fun doPermissionRequest(reqCode : Int, permission : String) {
+        val p = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+
+        if (p != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(permission), reqCode)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    fun addNewProjectButtonClick(v:View)
+    {
+        val intent = Intent(this, NewProjectActivity::class.java)
+
+        intent.putExtra(UrlParameter, urlString)
+
+        startActivityForResult(intent, AddNewProjectResult)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        doPermissionRequest(1, Manifest.permission.INTERNET)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -30,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
